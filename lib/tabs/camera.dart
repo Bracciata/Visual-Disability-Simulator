31b8b7be3fb52572039bae0e78601c5a5faeb63b
Thomas Braccia <bracciata@gmail.com>
2020-05-24 09:04:57 -0600

    Added Camera.

--- lib/tabs/camera.dart
+++ lib/tabs/camera.dart
@@ -1,50 +1,146 @@
-import 'dart:async';
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CamTab extends StatefulWidget {
+  const CamTab({Key key}) : super(key: key);
+
   @override
-  _CamPreviewState createState() => _CamPreviewState();
+  CameraScreenState createState() => CameraScreenState();
 }
 
-class _CamPreviewState extends State<CamTab> {
+class CameraScreenState extends State<CamTab>
+    with AutomaticKeepAliveClientMixin {
   CameraController _controller;
-  Future<void> _initCamFuture;
+  List<CameraDescription> _cameras;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
+    _initCamera();
     super.initState();
-    _initApp();
   }
 
-  _initApp() async {
-    final cameras = await availableCameras();
-    final firstCam = cameras.first;
-
-    _controller = CameraController(
-      firstCam,
-      ResolutionPreset.medium,
-    );
-
-    _initCamFuture = _controller.initialize();
+  Future<void> _initCamera() async {
+    _cameras = await availableCameras();
+    _controller = CameraController(_cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+    if (_controller != null) {
+      if (!_controller.value.isInitialized) {
+        return Container();
+      }
+    } else {
+      return const Center(
+        child: SizedBox(
+          width: 32,
+          height: 32,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    if (!_controller.value.isInitialized) {
+      return Container();
+    }
     return Scaffold(
-      body: FutureBuilder<void>(
-        future: _initCamFuture,
-        builder: (context, snapshot) {
-            return CameraPreview(_controller);
-        },
+      backgroundColor: Theme.of(context).backgroundColor,
+      key: _scaffoldKey,
+      extendBody: true,
+      body: Stack(
+        children: <Widget>[
+          _buildCameraPreview(),
+          Positioned(
+            top: 24.0,
+            left: 12.0,
+            child: IconButton(
+              icon: Icon(
+                Icons.switch_camera,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                _onCameraSwitch();
+              },
+            ),
+          ),
+          
+        ],
+      ),    );
+  }
+
+  Widget _buildCameraPreview() {
+    final size = MediaQuery.of(context).size;
+    return ClipRect(
+      child: Container(
+        child: Transform.scale(
+          scale: _controller.value.aspectRatio / size.aspectRatio,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: CameraPreview(_controller),
+            ),
+          ),
+        ),
       ),
     );
   }
+
+
+
+  Future<void> _onCameraSwitch() async {
+    final CameraDescription cameraDescription =
+        (_controller.description == _cameras[0]) ? _cameras[1] : _cameras[0];
+    if (_controller != null) {
+      await _controller.dispose();
+    }
+    _controller = CameraController(cameraDescription, ResolutionPreset.medium);
+    _controller.addListener(() {
+      if (mounted) setState(() {});
+      if (_controller.value.hasError) {
+        showInSnackBar('Camera error ${_controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await _controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void logError(String code, String message) =>
+      print('Error: $code\nError Message: $message');
+
+  @override
+  bool get wantKeepAlive => true;
 }
